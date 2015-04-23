8fec7661807f74a1c30be145b7645302aa823fa5
Ryan Z Bartlett <ryan@imageagent.net>
2015-04-23 10:17:17 -0400

    Updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,51 @@
 'use strict';
 
+// Tooltips
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+// Shrink Navbar on Scroll
+
+$(window).scroll(function(){
+
+  var navbar = $('#main-navigation');
+
+  if( $(document).scrollTop() > 110 ) {
+    navbar.removeClass('navbar-fat');
+  } else {
+    navbar.addClass('navbar-fat');
+  }
+
+});
+
+
+$(document).ready(function(){  
+
+  // SmoothScroll
+  function smoothScroll(x) {
+    $(x+' a[href*=#]').click(function() {
+      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+          return false;
+        }
+      }
+    });
+  }
+
+  var scrollItems = [
+    '.nav',
+    '.navbar-header'
+  ];
+
+  for ( var i = 0; i < scrollItems.length; i++ ) {
+    smoothScroll(scrollItems[i]);
+  }
+
 });
